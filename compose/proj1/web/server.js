ab0077022235f81005b2f872a2f0557f3a44438c
Namratha <namratha343@gmail.com>
2025-07-04 12:07:48 +0530

    updated

--- compose/proj1/web/server.js
+++ compose/proj1/web/server.js
@@ -2,6 +2,7 @@ const os = require('os');
 const express = require('express');
 const app = express();
 const redis = require('redis');
+
 const redisClient = redis.createClient({
   host: 'redis',
   port: 6379
@@ -9,16 +10,14 @@ const redisClient = redis.createClient({
 
 app.get('/', function(req, res) {
     redisClient.get('numVisits', function(err, numVisits) {
-        numVisitsToDisplay = parseInt(numVisits) + 1;
-        if (isNaN(numVisitsToDisplay)) {
-            numVisitsToDisplay = 1;
-        }
-       res.send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay);
-        numVisits++;
-        redisClient.set('numVisits', numVisits);
+        let numVisitsToDisplay = parseInt(numVisits) || 0;
+        numVisitsToDisplay += 1;
+
+        redisClient.set('numVisits', numVisitsToDisplay);
+        res.send(os.hostname() + ': Number of visits is: ' + numVisitsToDisplay);
     });
 });
 
-app.listen(5000, function() {
+app.listen(5000, '0.0.0.0', function() {
     console.log('Web application is listening on port 5000');
 });
